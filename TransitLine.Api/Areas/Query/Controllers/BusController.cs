e9f57737c457d033c094123b0421870fb1bbc36f
王敏张 <820432001@qq.com>
2016-01-19 17:45:37 +0800

    新增公交查询线路、站点API

--- TransitLine.Api/Areas/Query/Controllers/BusController.cs
+++ TransitLine.Api/Areas/Query/Controllers/BusController.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TransitLine.Common.Entity;
 
 namespace TransitLine.Api.Areas.Query.Controllers
 {
-    public class BusController : Controller
+    public class BusController : BaseController
     {
-        /// <summary>
-        ///可根据具体公交线路名称获取该公交线路的信息列表，
-        ///包括有其所有途径站点，所属公司，首末班车时间等信息
-        /// GET: /Query/Bus/
-        /// </summary>
-        /// <param name="bus">线路号</param>
-        /// <returns></returns>
-        public string Busline(string bus)
+        public JsonResult Busline(string bus)
         {
-            return "公交线路";
+            var stats = new List<TransitLine.Common.Entity.Line.Stats>();
+            stats.Add(new TransitLine.Common.Entity.Line.Stats { Id = "星塘公交中心", lat = 31.320503, lng = 120.746819 });
+            List<TransitLine.Common.Entity.Line.LineEntity> lines = new List<TransitLine.Common.Entity.Line.LineEntity>();
+            lines.Add(new TransitLine.Common.Entity.Line.LineEntity
+            {
+                Id = "110路(星塘公交中心-星塘公交中心)",
+                Info = "首末车:06:00-21:30; 票价1元（空调车加价1元，每年3、4、10、11月免收空调费） IC卡六折优惠",
+                Stats = stats
+            });
+            lines.Add(new TransitLine.Common.Entity.Line.LineEntity { Id = "110路下行(星塘公交中心-星塘公交中心)", Info = "首末车:06:00-21:30;" });
+            return AjaxResult(lines);
         }
 
-        /// <summary>
-        /// 根据具体公交站名获取所有路过该站点的公交线路。
-        /// </summary>
-        /// <param name="station">站台名称</param>
-        /// <returns></returns>
         public string Station(string station)
         {
             return "站台";
