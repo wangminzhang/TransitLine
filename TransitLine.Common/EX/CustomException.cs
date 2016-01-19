4c506386441515a6feac143253a6f08569d26890
王敏张 <820432001@qq.com>
2016-01-19 17:52:47 +0800

    自定义异常处理

---
+++ TransitLine.Common/EX/CustomException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Common.EX
+{
+    [Serializable]
+    public class CustomException : Exception, ISerializable
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public CustomException(string msg, int code)
+            : base()
+        {
+            this.message = msg;
+            this.code = code;
+        }
+
+    }
+}
