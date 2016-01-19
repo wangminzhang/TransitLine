a9046025b2bf8eb928767e28dd53416992e26d52
王敏张 <820432001@qq.com>
2016-01-19 17:52:28 +0800

    错误枚举

---
+++ TransitLine.Common/Enum/ErrorEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Common.Enum
+{
+    public enum ErrorEnum
+    {
+        请求成功=0,
+        内部异常=1,
+        没有登录=2,
+        没有权限=3
+    }
+}
