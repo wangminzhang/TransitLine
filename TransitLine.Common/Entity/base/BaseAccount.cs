83c8370c957359c72c45ca6f52651c59ea97aa96
王敏张 <820432001@qq.com>
2016-01-19 17:50:02 +0800

    新增账户基础类

---
+++ TransitLine.Common/Entity/base/BaseAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Common.Entity
+{
+    public class BaseAccount
+    {
+        public string Name { get; set; }
+        public string Passworld { get; set; }
+    }
+}
