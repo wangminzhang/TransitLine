5c0184f09d07668f902cc062a332bca692f77a18
王敏张 <820432001@qq.com>
2016-01-19 17:54:15 +0800

    权限账户实体

---
+++ TransitLine.Entity/Authorization/Account.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Entity.Authorization
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int IsDelete { get; set; }
+    }
+}
