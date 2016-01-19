7618aa8a8bf638a564881d69051c304d27407a5b
王敏张 <820432001@qq.com>
2016-01-19 17:50:33 +0800

    新增基础分页类

---
+++ TransitLine.Common/Entity/base/BasePage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Common.Entity
+{
+    class BasePage
+    {
+        /// <summary>
+        /// 当前页
+        /// </summary>
+        public int Index { get; set; }
+
+        /// <summary>
+        /// 每页条数
+        /// </summary>
+        public int Size { get; set; }
+
+        /// <summary>
+        /// 总条数
+        /// </summary>
+        public int Count { get; set; }
+    }
+}
