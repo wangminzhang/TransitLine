c3103caa01df0fd94e5259348b1ece8b192eda88
王敏张 <820432001@qq.com>
2016-01-19 17:49:11 +0800

    新增互联开放验证

---
+++ TransitLine.Common/Authorization/OpenValidate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransitLine.Common.Enum;
+using TransitLine.Common.EX;
+
+namespace TransitLine.Common.Authorization
+{
+    public class OpenValidate
+    {
+        public static bool AuthorizationValidate(Entity.RequestHander requestHander, Entity.BaseAccount baseAccount)
+        {
+            throw new CustomException("没有权限", (int)ErrorEnum.没有权限);
+        }
+
+
+        public static void IdentityValidate(Entity.RequestHander requestHander, Entity.BaseAccount baseAccount)
+        {
+
+            throw new CustomException("请先登录", (int)ErrorEnum.没有登录);
+            OpenValidate.AuthorizationValidate(requestHander, baseAccount);
+
+        }
+    }
+}
