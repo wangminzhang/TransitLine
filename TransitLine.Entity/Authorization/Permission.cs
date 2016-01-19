856e4906a266cafdd8f2ac656f98116bbedb472a
王敏张 <820432001@qq.com>
2016-01-19 17:54:29 +0800

    权限实体

---
+++ TransitLine.Entity/Authorization/Permission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Entity.Authorization
+{
+    public class Permission
+    {
+        public int Id { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+        public string Area { get; set; }
+        public string Description { get; set; }
+        public int IsDelete { get; set; }
+    }
+}
