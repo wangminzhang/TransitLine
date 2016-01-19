46b22bb650ce663a2e5b976028450d2328e2dd97
王敏张 <820432001@qq.com>
2016-01-19 17:51:14 +0800

    新增请求资源信息

---
+++ TransitLine.Common/Entity/base/RequestHander.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransitLine.Common.Entity
+{
+    public class RequestHander
+    {
+        public string area { get; set; }
+        public string controller { get; set; }
+        public string action { get; set; }
+    }
+}
